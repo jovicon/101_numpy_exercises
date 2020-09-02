a785ae31b8df592cd8cfb41b7e434b3c35be2bbf
JoseContreras <jocontrera16@gmail.com>
2020-09-01 22:56:42 -0400

    exercicie 3 -- How to create a boolean array?

--- numpy_101.py
+++ numpy_101.py
@@ -11,3 +11,19 @@ print(array_exercice_2)
 # full solution
 array_exercice_2 = np.arange(10)
 print(array_exercice_2)
+
+
+# exercice 3 - How to create a boolean array?
+# Create a 3×3 numpy array of all True’s
+
+# my solution
+array_boolean_3x3 = np.full([3,3],True)
+print(array_boolean_3x3)
+
+# solution
+array_boolean_3x3 = np.full([3,3],True, dtype=bool)
+print(array_boolean_3x3)
+
+# Alternate method:
+array_boolean_3x3 = np.ones((3,3), dtype=bool)
+print(array_boolean_3x3)
