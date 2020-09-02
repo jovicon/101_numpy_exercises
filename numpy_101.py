6c5cc380ca8e1163e636ee8f4f10ceec4cc76d5a
JoseContreras <jocontrera16@gmail.com>
2020-09-01 22:45:45 -0400

    exercice 2 -- how to create a 1D array?

--- numpy_101.py
+++ numpy_101.py
@@ -1,4 +1,13 @@
 import numpy as np
 
+# exercice 1 - version from numpy
 print(np.version.version)
 
+# exercice 2 - creating numpy array
+# my solution
+array_exercice_2 = np.array([0,1,2,3,4,5,6,7,8,9])
+print(array_exercice_2)
+
+# full solution
+array_exercice_2 = np.arange(10)
+print(array_exercice_2)
