f42bef8da9ee490525a873e40cbd6e108d0678cb
JoseContreras <jocontrera16@gmail.com>
2020-09-01 23:06:00 -0400

    exercice 4 -- How to extract items that satisfy a given condition from 1D array? - ready

--- numpy_101.py
+++ numpy_101.py
@@ -27,3 +27,14 @@ print(array_boolean_3x3)
 # Alternate method:
 array_boolean_3x3 = np.ones((3,3), dtype=bool)
 print(array_boolean_3x3)
+
+# exercice 4 - How to extract items that satisfy a given condition from 1D array?
+# Q. Extract all odd numbers from arr
+
+# input: arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+# output: array([1, 3, 5, 7, 9])
+
+# my solution - is the solution
+arr = np.arange(10)
+arr_output = (arr[arr%2 == 1])
+print(arr_output)
